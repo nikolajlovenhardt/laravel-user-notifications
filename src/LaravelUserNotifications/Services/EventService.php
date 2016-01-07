5777afe34d4f54c6606fb6fb6da88e6ecec39d3c
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2016-01-07 17:16:09 +0100

    EventService

--- src/LaravelUserNotifications/Services/EventService.php
+++ src/LaravelUserNotifications/Services/EventService.php
@@ -2,10 +2,18 @@
 
 namespace LaravelUserNotifications\Services;
 
+use Event;
+use stdClass;
+
 class EventService
 {
-    public function fire($event, array $data)
+    /**
+     * @param stdClass|string $event
+     * @param array|string|stdClass|null $data
+     * @return boolean
+     */
+    public function fire($event, $data = null)
     {
-
+        return Event::fire($event, $data);
     }
 }
